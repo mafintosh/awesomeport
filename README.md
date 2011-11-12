d1a64da177e163d7fa6270f78f87b666ff74e208
Mathias Buus <mathiasbuus@gmail.com>
2011-11-12 20:41:20 +0100

    commit uno

---
+++ README.md
@@ -0,0 +1,11 @@
+# awesomeport
+
+find a free and awesome looking port. just install it through npm:
+
+	npm install -g awesomeport
+
+and afterwards just go to your terminal and type:
+
+	awesomeport
+
+it will then find a good looking port for you, e.g. `6000` or `8888`
---
+++ index.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+var net = require('net');
+var available = function(port, callback) {
+	var server = net.createServer();
+
+	server.on('error', function() {
+		callback(null, false);
+	});
+	server.listen(port, function() {
+		server.close();
+		callback(null, true);
+	});
+};
+
+var ports = [];
+
+for (var i = 6; i < 12; i++) {
+	ports.push(i*1000);
+}
+for (var i = 6; i < 10; i++) {
+	ports.push(i*1111);
+}
+for (var i = 6; i < 10; i++) {
+	ports.push(i*1010);
+}
+for (var i = 6; i < 10; i++) {
+	ports.push(i*1001);
+}
+for (var i = 6; i < 12; i++) {
+	ports.push(i*1100);
+}
+for (var i = 6; i < 11; i++) {
+	ports.push(i*1110);
+}
+
+var from = 6000;
+var i = 0;
+
+var find = function() {
+	var current = ports[i++] || from++;
+
+	available(current, function(err, free) {
+		if (free) {
+			process.stdout.write(''+current+'\n', function() {
+				process.exit(0);
+			});
+			return;
+		}
+		find();
+	});	
+};
+
+find();
