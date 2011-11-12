97c57dfd77af36e306e43a8c8e8c7915285f1e23
Mathias Buus <mathiasbuus@gmail.com>
2011-11-12 21:11:46 +0100

    no more unsafe ports

--- index.js
+++ index.js
@@ -15,26 +15,26 @@ var available = function(port, callback) {
 
 var ports = [];
 
-for (var i = 6; i < 12; i++) {
+for (var i = 7; i < 12; i++) {
 	ports.push(i*1000);
 }
-for (var i = 6; i < 10; i++) {
+for (var i = 7; i < 10; i++) {
 	ports.push(i*1111);
 }
-for (var i = 6; i < 10; i++) {
+for (var i = 7; i < 10; i++) {
 	ports.push(i*1010);
 }
-for (var i = 6; i < 10; i++) {
+for (var i = 7; i < 10; i++) {
 	ports.push(i*1001);
 }
-for (var i = 6; i < 12; i++) {
+for (var i = 7; i < 12; i++) {
 	ports.push(i*1100);
 }
-for (var i = 6; i < 11; i++) {
+for (var i = 7; i < 11; i++) {
 	ports.push(i*1110);
 }
 
-var from = 6000;
+var from = 7000;
 var i = 0;
 
 var find = function() {
